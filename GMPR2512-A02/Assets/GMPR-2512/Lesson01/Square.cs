b3bd22aba19133059a6195293e65f3661b825543
NoIdeasXLVI <wesleyrchilds@gmail.com>
2026-01-12 14:48:45 -0700

    making sure this works
    jan 12 testing

---
+++ GMPR2512-A02/Assets/GMPR-2512/Lesson01/Square.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace GMPR2512.Lesson01
+{
+    public class Square : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            Debug.Log("testing");
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
